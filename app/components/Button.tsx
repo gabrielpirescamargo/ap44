c5015c80823943ddc0cf3b9f2d4a6f33ee5cfc3a
Gabriel Pires de Camargo <pires@ibm.com>
2024-01-27 12:02:34 -0300

    fix

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { IconType } from "react-icons";
+import { IconType } from 'react-icons';
 
 interface ButtonProps {
   label: string;
@@ -11,15 +11,15 @@ interface ButtonProps {
   icon?: IconType;
 }
 
-const Button: React.FC<ButtonProps> = ({ 
-  label, 
-  onClick, 
-  disabled, 
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
   outline,
   small,
   icon: Icon,
 }) => {
-  return ( 
+  return (
     <button
       disabled={disabled}
       onClick={onClick}
@@ -31,8 +31,8 @@ const Button: React.FC<ButtonProps> = ({
         hover:opacity-80
         transition
         w-full
-        ${outline ? 'bg-white' : 'bg-rose-500'}
-        ${outline ? 'border-black' : 'border-rose-500'}
+        ${outline ? 'bg-white' : 'bg-blue-500'}
+        ${outline ? 'border-black' : 'border-blue-500'}
         ${outline ? 'text-black' : 'text-white'}
         ${small ? 'text-sm' : 'text-md'}
         ${small ? 'py-1' : 'py-3'}
@@ -43,16 +43,16 @@ const Button: React.FC<ButtonProps> = ({
       {Icon && (
         <Icon
           size={24}
-          className="
+          className='
             absolute
             left-4
             top-3
-          "
+          '
         />
       )}
       {label}
     </button>
-   );
-}
- 
+  );
+};
+
 export default Button;
--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -94,7 +94,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
         src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.432709144335!2d-46.44046112465588!3d-24.01580167849032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1db5fcd3ec69%3A0x816138eff43fd588!2sIsla%20del%20castillo!5e0!3m2!1spt-BR!2sbr!4v1706364192783!5m2!1spt-BR!2sbr'
         width='600'
         height='450'
-        style={{ border: 'none' }}
+        style={{ border: 'none', width: '100%' }}
         // allowfullscreen=''
         loading='lazy'
         // referrerpolicy='no-referrer-when-downgrade'
